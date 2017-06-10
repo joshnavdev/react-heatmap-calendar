f3071bb47ee3ea081364e11840ee11c3b7dec48b
Joshua Navarro <joshua.navarro35@gmail.com>
2017-06-09 19:46:21 -0500

    init

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+
+import CalendarHeatmap from 'components/CalendarHeatmap';
+
+const views = [{
+  nameDisplay: 'Pedidos',
+  dataIndex: 'deliveries',
+  color: 'blue',
+  maxCount: 20,
+  tooltipText: (count, date) => {
+    return <span>{`${count} pedidos el ${date}`}</span>
+  }
+}, {
+  nameDisplay: 'Reseñas',
+  dataIndex: 'reviews',
+  color: 'green',
+  maxCount: 20,
+  tooltipText: (count, date) => {
+    return <span>{`${count} reseñas el ${date}`}</span>
+  }
+}, {
+  nameDisplay: 'Otros',
+  dataIndex: 'others',
+  maxCount: 20,
+  color: 'purple',
+}]
+
+const streakData = {
+  '2017-05-05': {
+    deliveries: 1,
+    reviews: 2,
+    others: 0,
+  },
+};
+
+const calendarHeatmapProps = {
+  fewerText: 'alto',
+  lowerText: 'bajo',
+  views,
+  streakData,
+  // TODO: Ask for month names
+}
+class App extends Component {
+  render() {
+    return (
+      <CalendarHeatmap {...calendarHeatmapProps} />
+    );
+  }
+}
+
+export default App;
